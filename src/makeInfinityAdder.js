ea6853c3dd4b4e81a29818004aa233aedcf70ad3
svladimir1010 <vladimireur@gmail.com>
2026-06-05 23:29:38 +0300

    Fix name function

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -4,7 +4,7 @@
  * @return {function}
  */
 
-function makeInfinityAdder() {
+function makeAdder() {
   let total = 0;
 
   return function adder(x) {
@@ -22,4 +22,4 @@ function makeInfinityAdder() {
   };
 }
 
-module.exports = makeInfinityAdder;
+module.exports = makeAdder;
